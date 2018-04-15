168f23baf94a9a70504fd759870ffecd1ca1a834
Abeer Upadhyay <ab.esquarer@gmail.com>
2018-04-15 20:25:43 +0530

    Add basic tests
    Signed-off-by: Abeer Upadhyay <ab.esquarer@gmail.com>

---
+++ tests/test_mutex.py
@@ -0,0 +1,48 @@
+import unittest
+import redis
+import time
+
+from redismutex import RedisMutex
+
+
+class TestRedisMutex(unittest.TestCase):
+
+    def setUp(self):
+
+        self.redis = redis.StrictRedis(host='localhost', port=6379, db=4)
+        self.mutex = RedisMutex(self.redis)
+        self.key = str(time.time())
+
+    def test_basic_flow(self):
+
+        self.mutex = self.mutex.acquire_lock(self.key)
+
+        # Check if key and value are set properly
+        self.assertEqual(self.mutex.key, self.key)
+        self.assertTrue(self.mutex.value)
+        self.assertIsInstance(self.mutex.value, str)
+        # Check if value is same as that in redis
+        redis_value = self.redis.get(self.key)
+        self.assertEqual(self.mutex.value, redis_value)
+
+        self.mutex = self.mutex.release_lock()
+
+        # Check if key and value are reset to None after unlock
+        self.assertIsNone(self.mutex.key)
+        self.assertIsNone(self.mutex.value)
+
+    def test_as_context_manager(self):
+
+        with self.mutex.acquire_lock(self.key):
+
+            # Check if key and value are set properly
+            self.assertEqual(self.mutex.key, self.key)
+            self.assertTrue(self.mutex.value)
+            self.assertIsInstance(self.mutex.value, str)
+            # Check if value is same as that in redis
+            redis_value = self.redis.get(self.key)
+            self.assertEqual(self.mutex.value, redis_value)
+
+        # Check if key and value are reset to None after unlock
+        self.assertIsNone(self.mutex.key)
+        self.assertIsNone(self.mutex.value)
