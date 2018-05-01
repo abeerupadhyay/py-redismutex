25d0225b4dab2fdebec3487c3199170d188b2e25
Abeer Upadhyay <ab.esquarer@gmail.com>
2018-05-01 08:30:30 +0530

    Update setup for pypi
    Signed-off-by: Abeer Upadhyay <ab.esquarer@gmail.com>

--- setup.py
+++ setup.py
@@ -7,21 +7,28 @@ with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as version:
 setup(
     name="redismutex",
     version=VERSION,
-    description="Python implementation of Mutex using Redis",
+    description="Python implementation of mutex using redis",
+    url="https://github.com/esquarer/py-redismutex",
     author="Abeer Upadhyay",
     author_email="ab.esquarer@gmail.com",
-    url="https://github.com/esquarer/py-redismutex",
-    packages=find_packages(),
-    include_package_data=True,
+    license='MIT',
+    keywords="python redis mutex",
+    packages=find_packages(exclude=['docs', 'tests*']),
     install_requires=[
         'future==0.16.0',
         'redis==2.10.6'
     ],
-    zip_safe=False,
-    keywords="python redis mutex",
+    project_urls={
+        'Source': 'https://github.com/esquarer/py-redismutex/',
+        'Documentation': 'http://py-redismutex.readthedocs.io/en/latest/',
+        'Tracker': 'https://github.com/esquarer/py-redismutex/issues/',
+    },
     classifiers=[
-        'License :: MIT',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
     ]
 )
